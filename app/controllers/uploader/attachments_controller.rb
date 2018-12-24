750b1957836cd2ad520275560be5d6a8e909630a
Gleb Tv <glebtv@gmail.com>
2018-12-24 20:01:56 +0300

    fixes

--- app/controllers/uploader/attachments_controller.rb
+++ app/controllers/uploader/attachments_controller.rb
@@ -1,16 +1,16 @@
 module Uploader
   class AttachmentsController < ActionController::Metal
     include AbstractController::Callbacks
-  
-    before_filter :find_klass
-    before_filter :find_asset, :only => [:destroy]
-    
+
+    before_action :find_klass
+    before_action :find_asset, :only => [:destroy]
+
     def create
       @asset = @klass.new(params[:asset])
       @asset.uploader_create(params, request)
       render_resourse(@asset, 201)
     end
-    
+
     def update
       @assets = Array.wrap(params[:assets] || [])
 
@@ -25,9 +25,9 @@ module Uploader
       @asset.uploader_destroy(params, request)
       render_json({success: true})
     end
-    
+
     protected
-    
+
       def find_klass
         @klass = Uploader.constantize(params[:klass])
         raise ActionController::RoutingError.new("Class not found #{params[:klass]}") if @klass.nil?
@@ -35,9 +35,9 @@ module Uploader
 
       def find_asset
         @asset = @klass.where(:public_token => params[:id]).first
-        raise ActionController::RoutingError.new("Asset not found by guid #{params[:id]}") if @asset.nil? 
+        raise ActionController::RoutingError.new("Asset not found by guid #{params[:id]}") if @asset.nil?
       end
-      
+
       def render_resourse(record, status = 200)
         if record.errors.empty?
           render_json({:files => Array.wrap(record.to_jq_upload)}, status)
@@ -45,7 +45,7 @@ module Uploader
           render_json(record.errors, 422)
         end
       end
-      
+
       def render_json(hash_or_object, status = 200)
         ctype = env["HTTP_USER_AGENT"] && env["HTTP_USER_AGENT"].include?("Android") ? "text/plain" : "application/json"
 
